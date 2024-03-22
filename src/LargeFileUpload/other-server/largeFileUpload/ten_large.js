28aa84e8cf8ab89952c24a7582e6130fb308df98
H <744769098@qq.com>
2024-03-22 18:00:36 +0800

    feat(server): 虚拟滚动

--- src/LargeFileUpload/other-server/largeFileUpload/ten_large.js
+++ src/LargeFileUpload/other-server/largeFileUpload/ten_large.js
@@ -14,10 +14,7 @@ const http = require('http');
 const Bucket = 'han-9999-1257066862';
 const Region = 'ap-nanjing';
 
-const cos = new COS({
-  SecretId: '', // 推荐使用环境变量获取；用户的 SecretId，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参考https://cloud.tencent.com/document/product/598/37140
-  SecretKey: '', // 推荐使用环境变量获取；用户的 SecretKey，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参考https://cloud.tencent.com/document/product/598/37140
-});
+const cos = new COS({});
 
 module.exports = class {
   async init(req, res) {
