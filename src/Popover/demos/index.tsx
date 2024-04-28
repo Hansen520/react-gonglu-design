dfb925eb82f2bd99221b2ed92e6154cef2191b0c
H <744769098@qq.com>
2024-04-28 15:50:54 +0800

    feat(server): 1

--- src/Popover/demos/index.tsx
+++ src/Popover/demos/index.tsx
@@ -1,26 +1,50 @@
-import { useFloating, useHover, useInteractions } from '@floating-ui/react';
+import {
+  offset,
+  useFloating,
+  useHover,
+  useInteractions,
+} from '@floating-ui/react';
 import { Button } from 'antd';
 import { useState } from 'react';
+import './index.less';
 
 function Demos() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const { refs, context } = useFloating({
+  const { refs, floatingStyles, context } = useFloating({
     open: isOpen,
     onOpenChange: setIsOpen,
     // 配置其他Floating UI的参数
+    placement: 'right',
+    middleware: [offset(10)],
   });
 
   const hover = useHover(context);
+  // const click = useClick(context);
+  // const dismiss = useDismiss(context);
 
-  const { getReferenceProps } = useInteractions([hover]);
+  const { getFloatingProps, getReferenceProps } = useInteractions([hover]);
 
   return (
-    <>
-      <Button ref={refs.setReference} {...getReferenceProps}>
+    <div style={{ position: 'relative' }}>
+      <Button
+        ref={refs.setReference}
+        {...getReferenceProps}
+        style={{ position: 'relative' }}
+      >
         hello
       </Button>
-    </>
+      {isOpen && (
+        <div
+          className="popover-floating"
+          ref={refs.setFloating}
+          style={floatingStyles}
+          {...getFloatingProps()}
+        >
+          hanHanHanHanHan
+        </div>
+      )}
+    </div>
   );
 }
 
